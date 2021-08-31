27ad570dc8d81aa106ed0facf10230a298540aa6
aircjm <aircjm@gmail.com>
2021-08-31 09:47:03 +0800

    feat(valid): add config

--- study-hibernate-validate/src/main/java/com/aircjm/study/validate/controller/HibernateController.java
+++ study-hibernate-validate/src/main/java/com/aircjm/study/validate/controller/HibernateController.java
@@ -3,9 +3,11 @@ package com.aircjm.study.validate.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 /**
  * @author aircjm
@@ -24,6 +26,13 @@ public class HibernateController {
     @GetMapping("/methodParamCheck")
     public String methodParamCheck(@Valid String code) {
         return code;
+    }
+
+
+    @Valid
+    @GetMapping("/testParamValid")
+    public String testParamValid(@RequestParam @NotNull Long code) {
+        return "success";
     }
 
 }
