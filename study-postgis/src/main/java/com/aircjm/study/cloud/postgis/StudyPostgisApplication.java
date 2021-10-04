275e6c95927002226af4de9a789d25e0d7f04829
aircjm <aircjm@gmail.com>
2021-10-04 09:52:42 +0800

    feat(pom): 修复原来问题

--- study-postgis/src/main/java/com/aircjm/study/cloud/postgis/StudyPostgisApplication.java
+++ study-postgis/src/main/java/com/aircjm/study/cloud/postgis/StudyPostgisApplication.java
@@ -1,13 +1,13 @@
-package com.aircjm.study.validate;
+package com.aircjm.study.cloud.postgis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudyHibernateValidateApplication {
+public class StudyPostgisApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StudyHibernateValidateApplication.class, args);
+        SpringApplication.run(StudyPostgisApplication.class, args);
     }
 
 }
