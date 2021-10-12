87cecd8fa88207bc17b6535c420e604564d153db
aircjm <aircjm@gmail.com>
2021-10-12 23:30:33 +0800

    feat(poi): poi使用

---
+++ study-cloud-web/src/main/java/com/aircjm/study/cloud/webconfig/utils/BaseImportEvo.java
@@ -0,0 +1,125 @@
+package com.aircjm.study.cloud.webconfig.utils;
+
+import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.json.JSONUtil;
+import com.aircjm.cloud.common.exceptions.BaseException;
+import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.context.AnalysisContext;
+import com.alibaba.excel.event.AnalysisEventListener;
+import com.alibaba.excel.exception.ExcelDataConvertException;
+import com.google.common.collect.Lists;
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * Excel基础导入对象-接收解析回调
+ * 提供基础导入的功能，如有特殊逻辑，可继承此类并覆写本类的方法 自定义实现
+ *
+ * @author aircjm
+ */
+@Getter
+@Setter
+@Slf4j
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@SuppressWarnings("unchecked")
+public class BaseImportEvo<T> extends AnalysisEventListener<T> {
+
+    /**
+     * 解析开始时间
+     */
+    private long startTime;
+
+    /**
+     * 解析Excel得到的数据结果集
+     */
+    private List<T> data = Lists.newArrayList();
+
+    public BaseImportEvo(long startTime) {
+        this.startTime = startTime;
+    }
+
+    /**
+     * 在转换异常 获取其他异常下会调用本接口。抛出异常则停止读取。如果这里不抛出异常则 继续读取下一行。
+     *
+     * @param exception 异常
+     * @param context   文本
+     * @throws Exception 异常
+     */
+    @Override
+    public void onException(Exception exception, AnalysisContext context) throws Exception {
+        log.warn("解析失败，但是继续解析下一行:{}", exception.getMessage());
+        // 如果是某一个单元格的转换异常 能获取到具体行号
+        // 如果要获取头的信息 配合invokeHeadMap使用
+        if (exception instanceof ExcelDataConvertException) {
+            ExcelDataConvertException excelDataConvertException = (ExcelDataConvertException) exception;
+            log.warn("第{}行，第{}列解析异常", excelDataConvertException.getRowIndex(),
+                    excelDataConvertException.getColumnIndex());
+        }
+        throw new BaseException("excel parse error", exception.getCause());
+    }
+
+    /**
+     * 这里会一行一行的返回头
+     *
+     * @param headMap 头元素
+     * @param context 文本
+     */
+    @Override
+    public void invokeHeadMap(Map<Integer, String> headMap, AnalysisContext context) {
+        log.info("解析到一条头数据:{}", JSONUtil.toJsonStr(headMap));
+    }
+
+
+    /**
+     * Excel的每一行解析完成回调此方法
+     *
+     * @param vo      对象
+     * @param context 文本
+     */
+    @Override
+    public void invoke(T vo, AnalysisContext context) {
+        log.info("解析到一条数据:{}", JSONUtil.toJsonStr(vo));
+        if (Objects.isNull(vo)) {
+            log.info("当前数据为空,不添加到data中");
+            return;
+        }
+        try {
+            List<Field> fields = ReflectUtil.getAllFieldsWithoutParent(vo.getClass());
+            for (Field field : fields) {
+                if (Objects.nonNull(field.getAnnotation(ExcelProperty.class))) {
+                    PropertyDescriptor propertyDescriptor = new PropertyDescriptor(field.getName(), vo.getClass());
+                    Object obj = propertyDescriptor.getReadMethod().invoke(vo);
+                    if (Objects.nonNull(obj)) {
+                        data.add(vo);
+                        break;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            log.warn("easyExcel removeNullData error", e);
+            // 发生异常则返回原始数据
+            data.add(vo);
+        }
+    }
+
+    /**
+     * Excel的所有数据解析完成回调此方法
+     *
+     * @param context 文本
+     */
+    @Override
+    public void doAfterAllAnalysed(AnalysisContext context) {
+        if (CollectionUtil.isEmpty(data)) {
+            throw new BaseException("解析结果集为空");
+        }
+        log.info("解析Excel完成,耗时:{}ms", System.currentTimeMillis() - startTime);
+    }
+}
---
+++ study-cloud-web/src/main/java/com/aircjm/study/cloud/webconfig/utils/ReflectUtil.java
@@ -0,0 +1,214 @@
+package com.aircjm.study.cloud.webconfig.utils;
+
+import com.aircjm.cloud.common.exceptions.Assert;
+import com.google.common.collect.Lists;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.beans.PropertyDescriptor;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * 反射工具类
+ * @author aircjm
+ */
+@Slf4j
+public class ReflectUtil {
+
+
+    /**
+     * 获取指定类的所有声明的字段，包括父类
+     *
+     * @param targetClass 指定的类型
+     * @return Field集合
+     */
+    public static List<Field> getAllFields(Class<?> targetClass) {
+        if (targetClass == null) {
+            return Collections.emptyList();
+        }
+        List<Field> fields = Lists.newArrayList();
+        while (targetClass != Object.class) {
+            fields.addAll(Arrays.asList(targetClass.getDeclaredFields()));
+            targetClass = targetClass.getSuperclass();
+        }
+        return fields;
+    }
+
+    /**
+     * 获取指定类的所有声明的字段，不包括父类
+     *
+     * @param targetClass 指定的类型
+     * @return Field集合
+     */
+    public static List<Field> getAllFieldsWithoutParent(Class<?> targetClass) {
+        if (targetClass == null) {
+            return Collections.emptyList();
+        }
+        List<Field> fields = Lists.newArrayList();
+        fields.addAll(Arrays.asList(targetClass.getDeclaredFields()));
+        return fields;
+    }
+
+    /**
+     * 获取所有属性值
+     * <p>
+     * getDeclaredField是可以获取一个类的所有字段
+     * getField只能获取类的public字段
+     *
+     * @param clazz 指定的对象
+     * @return 返回值
+     */
+    public static List<String> getAllFieldValue(Class<?> clazz) {
+        List<String> values = Lists.newArrayList();
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field : fields) {
+            try {
+                values.add((String) field.get(field.getName()));
+            } catch (IllegalAccessException e) {
+                log.warn("getAllFieldValue.IllegalAccessException", e);
+                return Lists.newArrayList();
+            }
+        }
+        return values;
+    }
+
+    /**
+     * 获取所有属性值-返回Object类型
+     * <p>
+     * getDeclaredField是可以获取一个类的所有字段
+     * getField只能获取类的public字段
+     *
+     * @param t 指定的对象
+     * @return 返回值
+     */
+    public static <T> List<Object> getAllFieldObjectValue(T t) {
+        List<Object> values = Lists.newArrayList();
+        Field[] fields = t.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            field.setAccessible(true);
+            try {
+                values.add(field.get(t));
+            } catch (Exception e) {
+                log.warn("getAllFieldObjectValue error", e);
+                return Lists.newArrayList();
+            }
+        }
+        return values;
+    }
+
+    /**
+     * 根据字段名称获取字段值
+     *
+     * @param t         对象
+     * @param fieldName 字段名称
+     * @param <T>       类型
+     * @return 字段值
+     */
+    public static <T> Object getFieldValueByName(T t, String fieldName) {
+        if (t == null || StringUtils.isBlank(fieldName)) {
+            return null;
+        }
+        try {
+            List<Field> fields = ReflectUtil.getAllFields(t.getClass());
+            for (Field field : fields) {
+                if (field.getName().equalsIgnoreCase(fieldName)) {
+                    PropertyDescriptor propertyDescriptor = new PropertyDescriptor(field.getName(), t.getClass());
+                    return propertyDescriptor.getReadMethod().invoke(t);
+                }
+            }
+        } catch (Exception e) {
+            log.warn("getFieldByName error.", e);
+            return null;
+        }
+        return null;
+    }
+
+    /**
+     * 根据字段名称获取字段值
+     *
+     * @param obj   对象实例
+     * @param field 属性
+     * @return 字段值
+     */
+    public static Object getFieldValueByName(Object obj, Field field) {
+        Assert.requireNonNull(obj, "obj不能为空");
+        Assert.requireNonNull(field, "field不能为空");
+        try {
+            if (!Modifier.isFinal(field.getModifiers())) {
+                PropertyDescriptor propertyDescriptor = new PropertyDescriptor(field.getName(), obj.getClass());
+                return propertyDescriptor.getReadMethod().invoke(obj);
+            }
+        } catch (Exception e) {
+            log.warn("ReflectUtil.getFieldValueByName error", e);
+        }
+        return null;
+    }
+
+    /**
+     * 根据字段名称设置字段值
+     *
+     * @param obj   对象实例
+     * @param field 属性
+     * @param value 值
+     */
+    public static void setFieldValueByName(Object obj, Field field, Object value) {
+        Assert.requireNonNull(obj, "class不能为空");
+        Assert.requireNonNull(field, "field不能为空");
+        try {
+            PropertyDescriptor propertyDescriptor = new PropertyDescriptor(field.getName(), obj.getClass());
+            propertyDescriptor.getWriteMethod().invoke(obj, value);
+        } catch (Exception e) {
+            log.warn("ReflectUtil.getFieldValueByName error", e);
+        }
+    }
+
+
+
+    /**
+     * 获取含有指定注解的字段Field
+     *
+     * @param t          原始对象
+     * @param annotation 注解对象类型
+     * @param <T>        原始对象类型
+     * @param <A>        注解对象类型
+     * @return Field集合
+     */
+    public static <T, A> List<Field> getAnnotationField(T t, Class<? extends Annotation> annotation) {
+        if (Objects.isNull(t) || Objects.isNull(annotation)) {
+            return null;
+        }
+        List<Field> list = getAllFields(t.getClass());
+        if (CollectionUtils.isEmpty(list)) {
+            return null;
+        }
+        List<Field> result = Lists.newArrayList();
+        for (Field field : list) {
+            if (field.isAnnotationPresent(annotation)) {
+                result.add(field);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 为所有字段设置相同的值
+     *
+     * @param t   原始对象
+     * @param v   值
+     * @param <T> 类型
+     * @param <V> 类型
+     */
+    public static <T, V> void setAllFieldsSameValue(T t, V v) {
+        List<Field> fields = ReflectUtil.getAllFieldsWithoutParent(t.getClass());
+        if (CollectionUtils.isNotEmpty(fields)) {
+            fields.forEach(field -> ReflectUtil.setFieldValueByName(t, field, v));
+        }
+    }
+}
