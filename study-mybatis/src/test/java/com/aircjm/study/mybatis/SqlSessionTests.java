77e2f8c9c926d70817324471acbbd12b5724a70c
aircjm <aircjm@gmail.com>
2022-04-02 16:38:51 +0800

    feat(init): mybatis 集成

--- study-mybatis/src/test/java/com/aircjm/study/mybatis/SqlSessionTests.java
+++ study-mybatis/src/test/java/com/aircjm/study/mybatis/SqlSessionTests.java
@@ -1,20 +1,33 @@
 package com.aircjm.study.mybatis;
 
+import cn.hutool.json.JSONUtil;
+import com.aircjm.study.mybatis.domain.UcUser;
+import com.aircjm.study.mybatis.mapper.UcUserMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class SqlSessionTests {
 
 
 
     @Test
-    public  void test() {
+    public  void test() throws IOException {
+        InputStream resourceAsStream = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
 
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(SqlSessionTests.class.getClassLoader().getResourceAsStream("mybatis-config.xml"));
+        SqlSession sqlSession = sqlSessionFactory.openSession();
 
+        UcUserMapper mapper = sqlSession.getMapper(UcUserMapper.class);
 
+        UcUser ucUser = mapper.selectOne(0L);
+        System.out.println(JSONUtil.toJsonStr(ucUser));
     }
 
 }
