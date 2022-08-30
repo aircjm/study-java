ce0299f3a4ffb861999dd55319dac5239e55342a
aircjm <aircjm@gmail.com>
2022-08-30 10:18:07 +0800

    feat(pom): 移除项目

--- juc-learn/src/main/java/com/aircjm/base/JucLearnApplication.java
+++ juc-learn/src/main/java/com/aircjm/base/JucLearnApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BaseJavaApplication {
+public class JucLearnApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BaseJavaApplication.class, args);
+        SpringApplication.run(JucLearnApplication.class, args);
     }
 
 }
