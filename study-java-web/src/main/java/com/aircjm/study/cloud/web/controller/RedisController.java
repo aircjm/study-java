3a544a2f5a6bf09f40018b068190458fa72a764d
aircjm <aircjm@gmail.com>
2023-03-01 14:51:32 +0800

    feat(redis): keys

--- study-java-web/src/main/java/com/aircjm/study/cloud/web/controller/RedisController.java
+++ study-java-web/src/main/java/com/aircjm/study/cloud/web/controller/RedisController.java
@@ -1,5 +1,7 @@
 package com.aircjm.study.cloud.web.controller;
 
+import com.rimlook.framework.core.pojo.Response;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/redis")
@@ -16,12 +20,29 @@ public class RedisController {
     @Resource
     private StringRedisTemplate stringRedisTemplate;
 
+    @Resource
+    private RedisTemplate  redisTemplate;
 
-    @GetMapping("testSetAndGet")
-    public String testSetAndGet() {
+    @GetMapping("/testIncrement")
+    public String testIncrement() {
         ValueOperations<String, String> operations = stringRedisTemplate.opsForValue();
         Long increment = operations.increment("test:inc");
         return String.valueOf(increment);
+    }
+
+    @GetMapping("/testSetAndGet")
+    public String testSetAndGet() {
+        Long increment = redisTemplate.boundValueOps("test:inc").increment();
+        return String.valueOf(increment);
+    }
+
+
+
+    @GetMapping("/keys")
+    public Response<Set<String>> keys() {
+        // todo keys no out print
+        Set<String> keys = redisTemplate.keys("*");
+        return Response.success(keys);
     }
 
 }
