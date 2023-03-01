00345720403daaa5a7697e071d6959d4ce410a42
aircjm <aircjm@gmail.com>
2023-03-01 14:09:11 +0800

    feat(redis): inc command

--- study-java-web/src/main/java/com/aircjm/study/cloud/web/controller/RedisController.java
+++ study-java-web/src/main/java/com/aircjm/study/cloud/web/controller/RedisController.java
@@ -1,25 +1,27 @@
 package com.aircjm.study.cloud.web.controller;
 
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
-@RestController("/redis")
+@RestController
+@RequestMapping("/redis")
 public class RedisController {
 
 
     @Resource
-    private RedisTemplate redisTemplate;
-
+    private StringRedisTemplate stringRedisTemplate;
 
 
     @GetMapping("testSetAndGet")
     public String testSetAndGet() {
-
-        return "redis test";
-
+        ValueOperations<String, String> operations = stringRedisTemplate.opsForValue();
+        Long increment = operations.increment("test:inc");
+        return String.valueOf(increment);
     }
 
 }
